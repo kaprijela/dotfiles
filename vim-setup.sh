ab5adeaa6b60be791ab3d97408bcb2c3a917a6e4
Gabriela Kandová <gabika.kandova@gmail.com>
2020-10-22 13:07:05 +0200

    Fix vim-setup according to shellcheck.net

--- vim-setup.sh
+++ vim-setup.sh
@@ -2,11 +2,11 @@
 AUTOLOAD_FOLDER=~/.vim/autoload
 PLUGIN_FOLDER=~/.vim/bundle
 PLUGINS=(
-	"vim-airline",
-	"vim-airline-themes",
-	"vim-gitgutter",
-	"vim-indent-guides",
-	"tabular",
+	"vim-airline"
+	"vim-airline-themes"
+	"vim-gitgutter"
+	"vim-indent-guides"
+	"tabular"
 	"palenight"
 )
 
@@ -14,25 +14,28 @@ PLUGINS=(
 mkdir -p $PLUGIN_FOLDER $AUTOLOAD_FOLDER
 
 # set up pathogen
-if [ !-e ${AUTOLOAD_FOLDER}/pathogen.vim ] then
+if [ ! -e ${AUTOLOAD_FOLDER}/pathogen.vim ]; then
 	curl -LSso ${AUTOLOAD_FOLDER}/pathogen.vim "https://tpo.pe/pathogen.vim"
 fi
 
 # set up plugins
-cd $PLUGIN_FOLDER
-for PLUGIN in ${PLUGINS[@]}; do
-	if [ !-e ${PLUGIN_FOLDER}/${PLUGIN} ] then
-		if [ $PLUGIN = "vim-airline" ] then
+cd $PLUGIN_FOLDER || exit
+
+for PLUGIN in "${PLUGINS[@]}"; do
+	if [ ! -e ${PLUGIN_FOLDER}/"${PLUGIN}" ]; then
+		echo "Preparing $PLUGIN"
+
+		if [ "$PLUGIN" = "vim-airline" ]; then
 			git clone "https://github.com/vim-airline/vim-airline"
-		elif [ $PLUGIN = "vim-airline-themes" ] then
+		elif [ "$PLUGIN" = "vim-airline-themes" ]; then
 			git clone "https://github.com/vim-airline/vim-airline-themes"
-		elif [ $PLUGIN = "vim-gitgutter" ] then
+		elif [ "$PLUGIN" = "vim-gitgutter" ]; then
 			git clone "https://github.com/airblade/vim-gitgutter"
-		elif [ $PLUGIN = "vim-indent-guides" ] then
+		elif [ "$PLUGIN" = "vim-indent-guides" ]; then
 			git clone "https://github.com/valloric/vim-indent-guides"
-		elif [ $PLUGIN = "tabular" ] then
+		elif [ "$PLUGIN" = "tabular" ]; then
 			git clone "https://github.com/godlygeek/tabular"
-		elif [ $PLUGIN = "palenight" ]
+		elif [ "$PLUGIN" = "palenight" ]; then
 			git clone "git clone https://github.com/drewtempelmeyer/palenight.vim"
 		fi
 	fi
